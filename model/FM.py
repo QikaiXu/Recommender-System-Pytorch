7c9496273e979fbf4fb44e9f5dc6a4dfcc605e1f
QikaiXu <691924095@qq.com>
2021-11-17 16:21:05 +0800

    FM 式子写错了，感谢 @cpy18727 纠正

--- model/FM.py
+++ model/FM.py
@@ -18,7 +18,7 @@ class FactorizationMachine(nn.Module):
         # embed(x) shape: (batch_size, num_fields, embed_dim)
         square_sum = self.embed2(x).sum(dim=1).pow(2).sum(dim=1)
         sum_square = self.embed2(x).pow(2).sum(dim=1).sum(dim=1)
-        output = self.embed1(x).squeeze(-1).sum(dim=1) + self.bias + (square_sum + sum_square) / 2
+        output = self.embed1(x).squeeze(-1).sum(dim=1) + self.bias + (square_sum - sum_square) / 2
         output = torch.sigmoid(output).unsqueeze(-1)
         return output
 
